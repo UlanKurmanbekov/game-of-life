0b8904d268fa7e8e82b993b239eb1402bb64d0bc
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2025-11-17 15:00:41 +0600

    Rewrite core logic using NumPy

--- main.py
+++ main.py
@@ -1,49 +1,45 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 import patterns
 
 
 class GameOfLife:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
-        self.grid = [[0] * width for _ in range(height)]
+        self.grid = np.zeros((width, height), dtype=np.uint8)
 
-    def update(self):
-        current_gen = [row[:] for row in self.grid]
-        next_gen = [row[:] for row in self.grid]
+    def update(self) -> None:
+        current_gen = self.grid
 
-        for i in range(self.height):
-            for j in range(self.width):
-                cnt = self.counter(i, j, current_gen)
+        neighbors = self._count_neighbors(current_gen)
+        birth_mask = (current_gen == 0) & (neighbors == 3)
+        underpopulation_mask = (current_gen == 1) & (neighbors < 2)
+        overpopulation_mask = (current_gen == 1) & (neighbors > 3)
 
-                if cnt == 3:
-                    next_gen[i][j] = 1
-                elif cnt < 2 or cnt > 3:
-                    next_gen[i][j] = 0
-
+        next_gen = self.grid.copy()
+        next_gen[birth_mask] = 1
+        next_gen[underpopulation_mask | overpopulation_mask] = 0
         self.grid = next_gen
 
-    def get_grid(self):
-        for vector in self.grid:
-            print(vector)
-
-        print('\n\n\n\n')
-
-    def seed(self, x, y):
+    def spawn_cell(self, x: int, y: int) -> None:
         self.grid[x][y] = 1
 
-    def counter(self, x, y, curr_gen: list[list[int]]):
-        cnt = 0
+    @staticmethod
+    def _count_neighbors(grid: np.ndarray) -> np.ndarray:
 
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if i == 0 and j == 0:
-                    continue
-                neighbor_x = (x + i) % self.height
-                neighbor_y = (y + j) % self.width
-                cnt += curr_gen[neighbor_x][neighbor_y]
-        return cnt
+        neighbors = (
+            np.roll(grid, 1, axis=0) +
+            np.roll(grid, 1, axis=1) +
+            np.roll(grid, -1, axis=0) +
+            np.roll(grid, -1, axis=1) +
+            np.roll(np.roll(grid, 1, axis=0), 1, axis=1) +
+            np.roll(np.roll(grid, 1, axis=0), -1, axis=1) +
+            np.roll(np.roll(grid, -1, axis=0), 1, axis=1) +
+            np.roll(np.roll(grid, -1, axis=0), -1, axis=1)
+        )
+        return neighbors
 
 
 width, height = list(map(int, input('Enter the field size (separated by a space): ').split())) or [50, 50]
@@ -57,7 +53,7 @@ yticks = [i - 0.5 for i in range(height + 1)]
 g = GameOfLife(width, height)
 
 for x, y in patterns.GLIDER:
-    g.seed(x, y)
+    g.spawn_cell(x, y)
 
 plt.ion()
 fig, ax = plt.subplots()
